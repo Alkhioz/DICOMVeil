a2b8c1cc3976ca72a37a8f46a7eed66abd73afda
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2024-05-06 23:39:17 -0500

    fixed file uploader, now you can upload individual DICOM files. Added sample anonymize and download buttons to file anonymizer component

--- dicomveilui/src/components/file-anonymizer.component.tsx
+++ dicomveilui/src/components/file-anonymizer.component.tsx
@@ -1,20 +1,44 @@
 import React from "react";
-import { fileElement } from "../hooks/useFiles.hook";
+import { fileElement, fileStatus } from "../hooks/useFiles.hook";
 
 export type FileAnonymizerProps = {
     files: fileElement[];
+    removeFile: (id: string) => void;
+    anonymizeFiles: (ids: string[]) => void;
+    downloadFiles: (ids: string[]) => void;
+    clearFiles: () => void;
 }
 
 export const FileAnonymizer = (props: FileAnonymizerProps) => {
+    const handleAnonymization = () => {
+        const filesToAnonymize = props?.files
+            ?.filter(file => file.status === fileStatus.UPLOADED)
+            ?.map(file => file.index);
+        props.anonymizeFiles(filesToAnonymize);
+    }
+    const handleDownload = () => {
+        const filesToDonwload = props?.files
+            ?.filter(file => file.status === fileStatus.ANONYMIZED)
+            ?.map(file => file.index);
+        props.downloadFiles(filesToDonwload);
+    }
     return (
         <div className="p-4 w-full h-full border border-blue-700">
             {
                 props.files?.map((current) =>
                     <React.Fragment key={current.index} >
-                       <p>{current.file.name}</p>
+                        <p>{current.file.name}</p>
                     </React.Fragment>
                 )
             }
+            <button
+                className="block p-4 bg-blue-700  text-white m-2"
+                onClick={handleAnonymization}
+            >Anonymize All</button>
+            <button
+                className="block p-4 bg-blue-700 text-white m-2"
+                onClick={handleDownload}
+            >Dowload All</button>
         </div>
     );
 }
