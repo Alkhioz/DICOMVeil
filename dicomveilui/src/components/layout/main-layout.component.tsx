a934fc39932c0cd1dd156d44fc2cb38f45c26d3b
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2024-06-11 12:08:18 -0500

    Added tags

--- dicomveilui/src/components/layout/main-layout.component.tsx
+++ dicomveilui/src/components/layout/main-layout.component.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from "react";
 export const MainLayout: React.FC<PropsWithChildren<unknown>> = ({ children }) => {
 
     return (
-        <div className="w-dvw h-dvh dark:bg-black dark:text-white flex flex-col">
+        <div className="box-border w-dvw h-dvh dark:bg-black dark:text-white flex flex-col">
             <nav className="bg-blue-700 p-2 text-white">
                 <ul>
                     <li>
--- dicomveilui/src/components/layout/triple-layout.component.tsx
+++ dicomveilui/src/components/layout/triple-layout.component.tsx
@@ -9,13 +9,13 @@ interface LayoutProps {
 export const TripleLayout: React.FC<LayoutProps> = ({ sidebarTop, sidebarBottom, mainContent }) => {
     return (
         <div className="h-full w-full grid grid-cols-1 grid-rows-[auto,1fr,1fr] md:grid-cols-[auto,1fr] md:grid-rows-[auto,1fr] border border-blue-700">
-            <div className="w-full h-28 md:w-60">
+            <div className="w-full h-28 md:w-80">
                 {sidebarTop}
             </div>
             <div className="w-full h-full md:row-span-2">
                 {mainContent}
             </div>
-            <div className="w-full h-full md:w-60">
+            <div className="w-full h-full md:w-80">
                 {sidebarBottom}
             </div>
         </div>
